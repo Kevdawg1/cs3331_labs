ae7cd8b75fe57ef5787f4f19f1c9b5886f2f2d20
Kevin Kam <kevin151@hotmail.my>
2018-03-29 10:53:16 +1100

    changes

--- lab_3/WebServer.py
+++ lab_3/WebServer.py
@@ -3,7 +3,7 @@ import threading
 import codecs
 
 bind_ip = "127.0.0.1"
-bind_port = 8000
+bind_port = 9997
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,8 +16,28 @@ print ("[*] Listening on %s:%d" %(bind_ip, bind_port))
 def handle_client (client_socket):
 	request = client_socket.recv(1024)
 	print ("[*] Received: %s" % request)
-	f=codecs.open("index.html", 'r')
-	client_socket.sendall(f.read())
+	request_method = request.split(' ')[0]
+	if (request_method == 'GET'):
+		file_requested = request.split(' ')[1]
+	# if ((file_requested == '/') or (file_requested == '/index.html')):
+		file_requested = 'index.html'
+	# file_requested = 'www' + file_requested
+	print ("[*] Serving web page: %s" % file_requested)
+	try:
+		file_handler = open(file_requested,'rb')
+		if (request_method == 'GET'):
+			response_content = file_handler.read()
+		file_handler.close()
+	except Exception as e:
+		print("Warning file not found. Serving response code 404")
+		if (request_method == 'GET'):
+			response_content = b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
+	server_response = request.encode()
+	if (request_method == 'GET'):
+		server_response += response_content
+	client_socket.send(server_response)
+	# f=codecs.open("index.html", 'r')
+	# client_socket.sendall(f.read())
 	client_socket.close()
 
 while True:
